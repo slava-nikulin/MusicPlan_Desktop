199cabd9973fa960db044588fbca1ce19c10feba
slava-nikulin <amphetomilk@gmail.com>
2015-02-09 18:11:30 +0300

    Listbox template changed. Prepared students viewmodel for schedule view

---
+++ MusicPlan.BLL/ExtendedModels/StudentsScheduleViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MusicPlan.BLL.Models;
+
+namespace MusicPlan.BLL.ExtendedModels
+{
+    public class StudentsScheduleViewModel
+    {
+        public List<SubjectsScheduleViewModel> AvailableSubjects { get; set; }
+        public Student Student { get; set; }
+
+        public StudentsScheduleViewModel(Student student, IEnumerable<Subject> subjects)
+        {
+            AvailableSubjects = new List<SubjectsScheduleViewModel>();
+            Student = student;
+            foreach (var subject in subjects)
+            {
+                AvailableSubjects.Add(new SubjectsScheduleViewModel(subject));
+            }
+        }
+    }
+
+    
+}
---
+++ MusicPlan.BLL/ExtendedModels/SubjectsScheduleViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MusicPlan.BLL.Models;
+
+namespace MusicPlan.BLL.ExtendedModels
+{
+    public class SubjectsScheduleViewModel
+    {
+        public List<Teacher> SelectedTeachers { get; set; }
+        public Subject Subject { get; set; }
+
+        public SubjectsScheduleViewModel(Subject subject)
+        {
+            SelectedTeachers = new List<Teacher>();
+            Subject = subject;
+        }
+    }
+}
