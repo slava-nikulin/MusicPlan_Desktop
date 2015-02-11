ca404cf823d98f13bad213763d3f4cd52312e195
slava-nikulin <amphetomilk@gmail.com>
2015-02-11 08:20:02 +0300

    Revert "Working on schedules 2"
    This reverts commit 4c8a244370239e59b40848798d0428e118254261.

---
+++ MusicPlan.BLL/ExtendedModels/StudentsScheduleViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MusicPlan.BLL.Models;
+
+namespace MusicPlan.BLL.ExtendedModels
+{
+    public class StudentsScheduleViewModel
+    {
+        public List<SubjectScheduleViewModel> AvailableSubjects { get; set; }
+        public Student Student { get; set; }
+
+        public StudentsScheduleViewModel(Student student, IEnumerable<Subject> subjects)
+        {
+            AvailableSubjects = new List<SubjectScheduleViewModel>();
+            Student = student;
+            foreach (var subject in subjects)
+            {
+                foreach (var param in subject.HoursParameters.Where(la=>la.StudyYear==student.StudyYear))
+                {
+                    AvailableSubjects.Add(new SubjectScheduleViewModel(subject, param));
+                }
+            }
+        }
+    }
+}
